925abb22cf958c5ac07ea6ebc12bfbb4cfc3775e
Bhavya-Budhia <bhavyabudhia@gmail.com>
2021-04-15 01:53:32 +0530

    First commit

---
+++ src/pst.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author intel
+ */
+class pst {
+
+    static void setString(int i, String call) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    static void execute() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
